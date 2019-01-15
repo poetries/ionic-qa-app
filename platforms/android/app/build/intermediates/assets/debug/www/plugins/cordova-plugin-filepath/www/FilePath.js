8166bac9e4022d5486bd0aec0c0c54d7ec0a3d76
poetries <jingguanliuye@gmail.com>
2019-01-15 15:35:15 +0800

    update

---
+++ platforms/android/app/build/intermediates/assets/debug/www/plugins/cordova-plugin-filepath/www/FilePath.js
@@ -0,0 +1,17 @@
+cordova.define("cordova-plugin-filepath.FilePath", function(require, exports, module) {
+var exec = require('cordova/exec');
+
+
+module.exports = {
+    /**
+     * Resolve native path for given content URL/path.
+     * @param {String} path  Content URL/path.
+     * @param successCallback  invoked with a native filesystem path string
+     * @param errorCallback  invoked if error occurs
+     */
+    resolveNativePath: function(path, successCallback, errorCallback) {
+        exec(successCallback, errorCallback, "FilePath", "resolveNativePath", [path]);
+    }
+};
+
+});
